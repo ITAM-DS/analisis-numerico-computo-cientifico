46971f9b9ffb4bc63a070baf6986b60e751f955c
Manuel Alejandro Rios Zagal <marz7002@gmail.com>
2017-02-13 00:52:18 +0000

    Tarea 2 Manuel Rios

---
+++ MNO/entrega_tareas_de_C/tarea2/manuel_rios/1.c
@@ -0,0 +1,21 @@
+// 1) Investiga lo que quieren decir las letras EOF. ¿Por qué se debe definir c como int
+
+// EOF significa "End of File", se indica ya que una nueva linea no es el final de un programa, sino solo de la linea
+// esto hace que no se terminen lo loops
+
+// Para que el EOF funcione, lo hace asignando un numero negativo a C, en este caso -1. De esta manera indica el final
+// del archivo y por esto "c" es definido como int.
+
+// 2) Modifica main para que sólo se llame una vez a la función getchar()
+
+
+#include <stdio.h>
+
+main(void){
+    int c;
+    while((c = getchar()) != EOF){
+        putchar(c);
+    } 
+}
+
+
