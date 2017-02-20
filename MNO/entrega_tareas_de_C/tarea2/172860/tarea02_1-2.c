32b54e985e9271ed3bc5e2bec962303dd39ef751
ixime <ixmezach@googlemail.com>
2017-02-20 12:57:05 -0600

    tarea 2

--- MNO/entrega_tareas_de_C/tarea2/172860/tarea02_1-2.c
+++ MNO/entrega_tareas_de_C/tarea2/172860/tarea02_1-2.c
@@ -5,7 +5,11 @@ Ixchel G. Meza Chávez
 
 */
 
-// 1) EOF es una constante simbólica que significa End Of File, y se usa para indicar que no se puede leer mas datos de la fuente de datos, la cual puede ser un archivo o el stream. Para causar un EOF se debe oprimir ctrl+D. En C los caracteres son enteros. La funcion getchar (que lee caracteres) regresa un caracter sin signo convertido a entero. En el caso de EOF, getchar regresa el valor simbólico EOF para indicar que se acabó el archivo. El valor para C de EOF es -1.
+// 1) EOF es una constante simbólica que significa End Of File, y se usa para indicar que no se puede leer mas datos 
+//    de la fuente de datos, la cual puede ser un archivo o el stream. Para causar un EOF se debe oprimir ctrl+D. 
+//    En C los caracteres son enteros. La funcion getchar (que lee caracteres) regresa un caracter sin signo convertido a entero. 
+//    En el caso de EOF, getchar regresa el valor simbólico EOF para indicar que se acabó el archivo. 
+//    El valor para C de EOF es -1.
 
 // 2) modificar main para que sólo se llame una vez la función getchar()
 #include <stdio.h>
