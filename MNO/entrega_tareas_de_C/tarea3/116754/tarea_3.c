211d206354bde56a2fe234fe25dcbd61430672b0
Ollin Demian <ollin.langle@ciencias.unam.mx>
2017-02-20 00:22:14 -0600

    Ahora sí ya todo jala bien

--- MNO/entrega_tareas_de_C/tarea3/116754/tarea_3.c
+++ MNO/entrega_tareas_de_C/tarea3/116754/tarea_3.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
-// Definición de la función de impresión de arreglos
 int imparreglos(int dimen){
   int i,j,filas,columnas;
   if(dimen == 1){
     int vector[] = {-1,-2,-3,-4,-5,-6,-7,-8,-9,-10};
     int *p;
-    int i;
     filas = sizeof(vector)/sizeof(vector[0]);
       printf("\nLas entradas impares del vector, son:\n");
       for(i=0;i<filas;i++){
@@ -15,12 +13,15 @@ int imparreglos(int dimen){
   else{
     int vector[][3] = {{-1,-2,-3},{-4,-5,-6}};
     int(*p)[][3];
-    int i,j;
     filas = sizeof(vector)/sizeof(vector[0]);
     columnas = sizeof(vector[0])/sizeof(vector[0][0]);
     printf("\nLas entradas impares del vector, son:\n");
-    for(i=0;i<(filas*columnas);i++){
-      if((i+j)%2 != 0)printf("p[%d] = %d\n", i, vector[i][j]);
+    int k = 0;
+    for(i=0;i<filas;i++){
+      for(j=0;j<columnas;j++){
+        if((i+j)%2 != 0)printf("p[%d] = %d\n", k, vector[i][j]);
+        k++;
+      }
     }
   }
 }
@@ -30,7 +31,6 @@ int imdireccion(int dimen){
   if(dimen == 1){
     int vector[] = {-1,-2,-3,-4,-5,-6,-7,-8,-9,-10};
     int *p;
-    int i;
     filas = sizeof(vector)/sizeof(vector[0]);
     printf("\nLas direcciones de las entradas impares del vector, son:\n");
     for(i=0;i<filas;i++){
@@ -40,12 +40,15 @@ int imdireccion(int dimen){
   else{
     int vector[][3] = {{-1,-2,-3},{-4,-5,-6}};
     int(*p)[][3];
-    int i,j;
     filas = sizeof(vector)/sizeof(vector[0]);
     columnas = sizeof(vector[0])/sizeof(vector[0][0]);
     printf("\nLas direcciones de las entradas impares del vector, son:\n");
-    for(i=0;i<(filas*columnas);i++){
-      if((i)%2 != 0)printf("&p[%d] = %p\n", i, &vector[i]);
+    int k = 0;
+    for(i=0;i<filas;i++){
+      for(j=0;j<columnas;j++){
+        if((i+j)%2 != 0)printf("&p[%d] = %p\n", k, &vector[i][j]);
+        k++;
+      }
     }
   }
 }
