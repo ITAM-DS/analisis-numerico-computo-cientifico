ff8acf0229a3af4561380ca79f9056d990c4782a
ricardolastra <ricardolcuevas@gmail.com>
2017-02-21 17:05:42 -0600

    Mejore Output arreglo 2dim

--- MNO/entrega_tareas_de_C/tarea3/160167/tarea3F1y2.c
+++ MNO/entrega_tareas_de_C/tarea3/160167/tarea3F1y2.c
@@ -6,37 +6,47 @@
 #include<stdio.h>
 void main(){
     int arreglo[10] = {-1,-2,-3,-4,-5,-6,-7,-8,-9,-10};
-    int i,n;
-    int arreglom[2][5] = {{-1,-2,-3,-4,-5},{-6,-7,-8,-9,-10}};
-    printf("---todas las entradas---\n");
+    int i,j;
+    int arreglom[2][3] = {{-1,-2,-3},{-4,-5,-6}};
+    /* imprimiendo las entradas de 1 dim*/
+
+    printf("----todas las entradas----\n");
     for(i=0;i<sizeof(arreglo)/sizeof(arreglo[0]);i++)
         printf("arreglo[%d]=%d\n", i,arreglo[i]);
-    printf("--------------------\n");
+    printf("----direccion de memoria----\n");
     for(i=0;i<sizeof(arreglo)/sizeof(arreglo[0]);i++)
         printf("posicion arreglo %d, memoria: %p\n", i, arreglo+i);
-    printf("---solo entradas impares---\n");
+    printf("----solo entradas impares----\n");
     /* imprimiendo solo las entradas nones */
     for (i=0; i<sizeof(arreglo)/sizeof(arreglo[0]); i++) {
 	if (i % 2 != 0)
 		printf("arreglo[%d]=%d\n", i,arreglo[i]);
 	}
-    printf("------solo posiciones impares-----------\n");
+    printf("----solo posiciones impares----\n");
     for(i=0;i<sizeof(arreglo)/sizeof(arreglo[0]);i++) {
 	if (i % 2 != 0)
 		printf("posicion %d, memoria: %p\n", i, arreglo+i);
 	}
-    printf("---todas las entradas dos dim---\n");
-    for(i=0;i<sizeof(arreglom)/sizeof(arreglom[0][0]);i++)
-        printf("arreglom[%d][%d]=%d\n", i,arreglom[i]);
-    printf("--------------------\n");
+    /* imprimiendo las entradas de dos dim */
+
+    printf("----todas las entradas dos dim----\n");
+    for(i=0;i<sizeof(arreglom)/sizeof(arreglom[0]);i++){
+	for(j=0;j<sizeof(arreglom[0])/sizeof(arreglom[0][0]);j++)
+		printf("arreglom[%d][%d] = %d\n", i,j, arreglom[i][j]);
+	}
+    printf("----direccion de memoria dos dim----\n");
     for(i=0;i<sizeof(arreglom)/sizeof(arreglom[0][0]);i++)
         printf("posicion arreglo dos dim %d, memoria: %p\n", i,arreglom+i);
-    printf("---solo entradas impares dos dim---\n");
-    /* imprimiendo solo las entradas nones */
-    for (i=0; i<sizeof(arreglom)/sizeof(arreglom[0][0]); i++) {
-	if (i % 2 != 0)
-		printf("arreglom[%d][%d]=%d\n", i,arreglom[i]);
+    /* imprimiendo solo las posiciones nones de dos dim*/
+    printf("----solo entradas impares dos dim----\n");
+    for(i=0; i<sizeof(arreglom)/sizeof(arreglom[0]);i++){
+	for(j=0; j<sizeof(arreglom[0])/sizeof(arreglom[0][0]);j++){
+		if ((i+j) % 2 == 1){
+		printf("arreglom[%d][%d] = %d\n", i,j,arreglom[i][j]);
+		}
 	}
+	}
+    printf("----solo posiciones impares dos dim----\n");
     for(i=0;i<sizeof(arreglom)/sizeof(arreglom[0][0]);i++) {
 	if (i % 2 != 0)
 		printf("posicion %d, memoria: %p\n", i, arreglom+i);
