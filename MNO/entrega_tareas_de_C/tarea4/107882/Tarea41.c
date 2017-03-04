27ebcf48a047b5474395c255f8d9f5051c35ad73
walterMS <wsantana21eysdl@gmail.com>
2017-03-03 23:33:17 -0600

    Subiendo archivos tarea 4 y códigos

---
+++ MNO/entrega_tareas_de_C/tarea4/107882/Tarea41.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+#define MAX_LONG 200
+#define CADENA_PRUEBA "Hola a todos"
+
+int longitud_string(char * s){
+    int i=0;
+    while(*(s++) != '\0')
+        i++;
+    return i;
+}
+
+int main(void){
+    char string1[] = CADENA_PRUEBA; //definición y declaracion de variable e inicializacion.
+    char string2[MAX_LONG]; //definición y declaracion.
+    
+    printf("cadena: %s\n", string1);
+    printf("longitud cadena: %d\n", longitud_string(string1));
+    strcpy(string2, "leer libros y revistas"); //inicializacion de string2
+    printf("cadena2: %s\n", string2);
+    printf("longitud cadena: %d\n", longitud_string(string2));
+return 0;
+}
