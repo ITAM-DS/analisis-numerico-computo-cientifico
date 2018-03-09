3458db2810b80a412708a6f94b7b0c060daa087f
Alex Nivi <a.nivon.r@gmail.com>
2018-03-08 22:35:02 -0600

    ejercicio3 completo

--- entrega_tareas_de_C/tarea4/109649/ejercicio3.c
+++ entrega_tareas_de_C/tarea4/109649/ejercicio3.c
@@ -9,14 +9,34 @@ char *corta_string(char *apuntador){
     return apuntador;
 }
 
+void corta_espacio(char *apuntador){
+    int i = 1;
+    int flag = 0;
+    char c;
+    while((c=*(apuntador+i)) != '\0'){
+        if(flag==1) printf("%c", c);
+        if(c==' ') flag=1;
+        i++;  
+    } 
+}
+
 int main(void){
+    printf("\na) La funcion corta_string no hace un chequeo si el string que recibe\ncomo parámetro debe de tener al menos un caracter.");
+    printf(" Modifica esta función de modo que haga este chequeo:\n\n");
     char *s="Campos verdes";
     char *er="c";
+    printf("\nSe evalúa el string: Campos Verdes\n");
     s=corta_string(s);
+    printf("Resultado: %s\n\n",s);
+    printf("Se evalúa el string: c\n");
     er=corta_string(er);
-    printf("%s\n",s);
-    printf("%s\n", er);
-    
+    printf("Resultado: %s\n", er);
+
+    printf("\n\nb) Cómo modificas la forma en que es llamada corta_string dentro de main de modo que devuelva: verdes ?? (hay un espacio antes del string 'verdes').\n\n");
+    s="Campos verdes";
+    printf("Resultado: ");
+    corta_espacio(s);
+    printf("\n");
  
 return 0;
 }
