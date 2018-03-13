f2a72f9d2314d6c0c8564401885862d4592f9937
Carlos Castro Correa <carloscastrocorrea@PICTOLINE8005.local>
2018-03-12 22:07:46 -0600

    Tarea4

---
+++ entrega_tareas_de_C/tarea4/103531/tarea4_je2.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#define MAXLINEA 1000
+int max;
+char linea[MAXLINEA];
+char linea_max[MAXLINEA];
+
+int obtenlinea(void);
+void copia(void);
+
+int main(void){
+    int longitud;
+    extern int max;
+    extern char linea_max[MAXLINEA];
+    max=0;
+    while((longitud = obtenlinea()) > 0)
+        if( longitud > max){
+            max = longitud;
+            copia();
+        }
+    if(max >= 80)
+        printf("%s", linea_max);
+return 0;
+}
+
+int obtenlinea(void){
+    int c;
+    int i;
+    extern char linea[];
+    for(i=0; i < MAXLINEA-1 && (c=getchar())!=EOF && c!= '\n'; i++)
+        linea[i] = c;
+    if(c == '\n'){
+        linea[i] = c;
+        i++;
+    }
+    linea[i]='\0'; //este caracter es necesario para almacenar strings
+    return i;
+}
+
+void copia(void){
+    int i;
+    extern char linea[], linea_max[];
+    i=0;
+    while(1){
+        linea_max[i] = linea[i];
+        if(linea_max[i] == '\0') break;
+        i++;
+    }
+}
