adaa7208816ba8336abf0aa7391e482f383aab30
Mirtha Ayala <mirtha_aym@hotmail.com>
2018-05-02 21:20:50 -0500

    Renombra y corrige la funcion para calcular el promedio

---
+++ proyecto_final/proyectos/equipos/equipo_07/avance_02_05_2018/matrix.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h> 
+#include <csvparser.h>
+
+typedef struct matrix {
+  size_t cols;
+  size_t rows;
+  CsvRow **vectors;
+} matrix;
+
