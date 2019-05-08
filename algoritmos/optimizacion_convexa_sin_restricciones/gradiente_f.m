62d053c1e0dca053efb4bb88a69b5fc09416309d
Erick Palacios Moreno <palmoreck@gmail.com>
2019-05-08 18:07:09 -0500

    aproximate gradient of function

---
+++ algoritmos/optimizacion_convexa_sin_restricciones/gradiente_f.m
@@ -0,0 +1,20 @@
+function grad_f = gradiente_f(f,x)
+%In:
+%f función a la que se le calculará su gradiente
+%x punto en el que se aproximará el gradiente de f
+%Out:
+%grad_f: aproximación al gradiente de f por diferencias centradas
+h=1e-6;
+n=length(x);
+grad_f=zeros(n,1);
+xaux=x;
+xaux2=x;
+for i=1:n
+xaux(i)=x(i)+h;
+xaux2(i)=x(i)-h;
+grad_f(i) = (f(xaux)-f(xaux2))/(2*h);
+xaux(i)=xaux(i)-h;
+xaux2(i)=xaux2(i)+h;
+end
+ 
+end
