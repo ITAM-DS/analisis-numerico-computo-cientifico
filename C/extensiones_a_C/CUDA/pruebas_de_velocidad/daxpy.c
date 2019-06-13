2b2bb42e813a1f0c01d8dab140149efb62cca855
Erick Palacios Moreno <palmoreck@gmail.com>
2019-06-13 17:04:35 -0500

    daxpy

---
+++ C/extensiones_a_C/CUDA/pruebas_de_velocidad/daxpy.c
@@ -0,0 +1,45 @@
+//gcc -Wall daxpy.c funciones.c -o daxpy.out -lblas
+//./daxpy.out 6
+
+#include<stdio.h>
+#include<stdlib.h>
+#include"definiciones.h"
+
+extern double daxpy_(int *n, double *x, int *incx, double *y, int *incy);
+int main(int argc, char *argv[]){
+	arreglo_1d_T v1, v2;
+	int incx=1;
+	double alpha=3.5;
+	int N=atoi(argv[1]);
+
+	v1=malloc(sizeof(*v1));
+	v2=malloc(sizeof(*v2));
+
+	renglones_vector(v1)=N;
+	renglones_vector(v2)=N;
+
+	entradas_vector(v1)=calloc(N,sizeof(double));
+	entradas_vector(v2)=calloc(N,sizeof(double));
+
+	for(i=0;i<N;i++){
+		entrada_vector(v1,i) = i;
+		entrada_vector(v2,i) = i*i;
+	}
+
+	printf("------------\n");
+	printf("vector :\n");
+	imprime_vector(v1);
+	printf("------------\n");
+	printf("vector :\n");
+	imprime_vector(v2);
+	printf("------------\n");
+
+	daxpy_(&N, &alpha, entradas_vector(v1), &incx, entradas_vector(v2), &incx);
+	printf("resultado\n");
+	imprime_vector(v2);
+	free(entradas_vector(v1));
+	free(v1);
+	free(entradas_vector(v2));
+	free(v2);
+	return 0;
+}
