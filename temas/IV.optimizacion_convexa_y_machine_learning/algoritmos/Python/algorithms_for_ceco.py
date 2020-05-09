a7840ea68f54c78e79e16f5816ab8b77bc816a76
Erick <palmoreck@gmail.com>
2020-05-09 13:33:36 -0500

    testing implementation with more examples and adding symbolic gf and Hf

--- temas/IV.optimizacion_convexa_y_machine_learning/algoritmos/Python/algorithms_for_ceco.py
+++ temas/IV.optimizacion_convexa_y_machine_learning/algoritmos/Python/algorithms_for_ceco.py
@@ -9,18 +9,25 @@ from utils import compute_error
 
 
 def Newtons_method(f, A, x_0, tol, 
-                   tol_backtracking, x_ast=None, p_ast=None, maxiter=30):
+                   tol_backtracking, x_ast=None, p_ast=None, maxiter=30,
+                   gf_symbolic = None,
+                   Hf_symbolic = None):
     '''
-    Newton's method to numerically approximate solution of min f.
+    Newton's method to numerically approximate solution of min f subject to Ax = b.
+    IMPORTANT: this implementation requires that initial point x_0, satisfies: Ax_0 = b
     Args:
-        f (lambda expression): definition of function f.
+        f (fun): definition of function f as lambda expression or function definition.
         A (numpy ndarray): 2d numpy array of shape (m,n) defines system of constraints Ax=b.
-        x_0 (numpy ndarray): initial point for Newton's method.
+        x_0 (numpy ndarray): initial point for Newton's method. Must satisfy: Ax_0 = b
         tol (float): tolerance that will halt method. Controls stopping criteria.
         tol_backtracking (float): tolerance that will halt method. Controls value of line search by backtracking.
         x_ast (numpy ndarray): solution of min f, now it's required that user knows the solution...
         p_ast (float): value of f(x_ast), now it's required that user knows the solution...
         maxiter (int): maximum number of iterations
+        gf_symbolic (fun): definition of gradient of f. If given, no approximation is
+                                     performed via finite differences.
+        Hf_symbolic (fun): definition of Hessian of f. If given, no approximation is
+                                     performed via finite differences.
     Returns:
         x (numpy ndarray): numpy array, approximation of x_ast.
         iteration (int): number of iterations.
@@ -34,14 +41,22 @@ def Newtons_method(f, A, x_0, tol,
     x = x_0
     
     feval = f(x)
-    gfeval = gradient_approximation(f,x)
-    Hfeval = Hessian_approximation(f,x)
+    
+    if gf_symbolic:
+        gfeval = gf_symbolic(x)
+    else:
+        gfeval = gradient_approximation(f,x)
+
+    if Hf_symbolic:
+        Hfeval = Hf_symbolic(x)
+    else:
+        Hfeval = Hessian_approximation(f,x)
     
     normgf = np.linalg.norm(gfeval)
     condHf= np.linalg.cond(Hfeval)
     
     Err_plot_aux = np.zeros(maxiter)
-    Err_plot_aux[iteration]=math.fabs(feval-p_ast)
+    Err_plot_aux[iteration]=compute_error(p_ast,feval)
     
     Err = compute_error(x_ast,x)
     
@@ -54,32 +69,30 @@ def Newtons_method(f, A, x_0, tol,
         second_stack = np.row_stack((A.reshape(1,n).T,zero_matrix)).reshape(1,n+1)[0]
     else:
         m,n = A.shape
-        zero_matrix = np.zeros((m,n))
+        zero_matrix = np.zeros((m,m))
         first_stack = np.column_stack((Hfeval, A.T))
-        second_stack = np.row_stack((A,zero_matrix))
+        second_stack = np.column_stack((A,zero_matrix))
         
     x_plot = np.zeros((n,maxiter))
     x_plot[:,iteration] = x
     
     system_matrix = np.row_stack((first_stack,second_stack))
     zero_vector = np.zeros(m)
-    rhs = np.row_stack((gfeval.reshape(n,1), zero_vector)).T[0]
+    rhs = np.row_stack((gfeval.reshape(n,1), zero_vector.reshape(m,1))).T[0]
 
     #Newton's direction and Newton's decrement
-    dir_desc = np.linalg.solve(system_matrix, rhs)
+    dir_desc = np.linalg.solve(system_matrix, -rhs)
     dir_Newton = dir_desc[0:n]
-    dec_Newton = dir_Newton.dot(Hfeval@dir_Newton)
-    
-    w_dual_variable_estimation = -dir_desc[n:(n+m)]
+    dec_Newton = -gfeval.dot(dir_Newton)
+    w_dual_variable_estimation = dir_desc[n:(n+m)]
 
-    dir_Newton = -dir_desc[0:n]
 
+    print('I\tNormgf \tNewton Decrement\tError x_ast\tError p_ast\tline search\tCondHf')
+    print('{}\t{:0.2e}\t{:0.2e}\t{:0.2e}\t{:0.2e}\t{}\t\t{:0.2e}'.format(iteration,normgf,
+                                                                         dec_Newton,Err,
+                                                                         Err_plot_aux[iteration],"---",
+                                                                         condHf))
     
-    print('I    Normgf   Newton Decrement  Error x_ast   Error p_ast   line search   Condition of Hessian')
-    print('{}    {:0.2e}    {:0.2e}        {:0.2e}      {:0.2e}       {}           {:0.2e}'.format(iteration,normgf,
-                                                                                                   dec_Newton,Err,
-                                                                                                   Err_plot_aux[iteration],"---",
-                                                                                                   condHf))
     stopping_criteria = dec_Newton/2
     iteration+=1
     while(stopping_criteria>tol and iteration < maxiter):
@@ -87,10 +100,17 @@ def Newtons_method(f, A, x_0, tol,
         t = line_search_by_backtracking(f,dir_Newton,x,der_direct)
         x = x + t*dir_Newton
         feval = f(x)
-        gfeval = gradient_approximation(f,x)
-        Hfeval = Hessian_approximation(f,x)
-        normgf = np.linalg.norm(gfeval)
-        condHf= np.linalg.cond(Hfeval)
+        
+        
+        if gf_symbolic:
+            gfeval = gf_symbolic(x)
+        else:
+            gfeval = gradient_approximation(f,x)
+        
+        if Hf_symbolic:
+            Hfeval = Hf_symbolic(x)
+        else:
+            Hfeval = Hessian_approximation(f,x)
         if(A.ndim == 1):
             m = 1
             n = x.size
@@ -99,27 +119,25 @@ def Newtons_method(f, A, x_0, tol,
             second_stack = np.row_stack((A.reshape(1,n).T,zero_matrix)).reshape(1,n+1)[0]
         else:
             m,n = A.shape
-            zero_matrix = np.zeros((m,n))
+            zero_matrix = np.zeros((m,m))
             first_stack = np.column_stack((Hfeval, A.T))
-            second_stack = np.row_stack((A,zero_matrix))
+            second_stack = np.column_stack((A,zero_matrix))
 
         system_matrix = np.row_stack((first_stack,second_stack))
-        rhs = np.row_stack((gfeval.reshape(n,1), zero_vector)).T[0]
-        
+        rhs = np.row_stack((gfeval.reshape(n,1), zero_vector.reshape(m,1))).T[0]
         #Newton's direction and Newton's decrement
-        dir_desc = np.linalg.solve(system_matrix, rhs)
+        dir_desc = np.linalg.solve(system_matrix, -rhs)
         dir_Newton = dir_desc[0:n]
-        dec_Newton = dir_Newton.dot(Hfeval@dir_Newton)
-        w_dual_variable_estimation = -dir_desc[n:(n+m)]
-        dir_Newton = -dir_desc[0:n] 
+        dec_Newton = -gfeval.dot(dir_Newton)
+        w_dual_variable_estimation = dir_desc[n:(n+m)]
         
-        Err_plot_aux[iteration] = math.fabs(feval-p_ast);
+        Err_plot_aux[iteration]=compute_error(p_ast,feval)
         x_plot[:,iteration] = x
         Err = compute_error(x_ast,x)
-        print('{}    {:0.2e}    {:0.2e}        {:0.2e}      {:0.2e}       {:0.2e}      {:0.2e}'.format(iteration,normgf,
-                                                                                                         dec_Newton,Err,
-                                                                                                         Err_plot_aux[iteration],t,
-                                                                                                         condHf))
+        print('{}\t{:0.2e}\t{:0.2e}\t{:0.2e}\t{:0.2e}\t{:0.2e}\t{:0.2e}'.format(iteration,normgf,
+                                                                                dec_Newton,Err,
+                                                                                Err_plot_aux[iteration],t,
+                                                                                condHf))
         stopping_criteria = dec_Newton/2
         if t<tol_backtracking: #if t is less than tol_backtracking then we need to check the reason
             iter_salida=iteration
