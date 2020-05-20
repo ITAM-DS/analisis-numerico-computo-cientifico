927d047e817dea022e969640ec7e905dc04ad4c0
Erick <palmoreck@gmail.com>
2020-05-20 11:59:51 -0500

    adding py archive

---
+++ temas/IV.optimizacion_convexa_y_machine_learning/algoritmos/Python/algorithms_for_cieco.py
@@ -0,0 +1,224 @@
+import math
+
+import numpy as np
+
+from utils import compute_error, logarithmic_barrier, constraint_inequalities_funcs_eval
+from line_search import line_search_for_log_barrier_by_backtracking 
+from numerical_differentiation import numerical_differentiation_of_logarithmic_barrier
+
+def path_following_method(f, A, constraints_ineq,
+                          x_0, tol,
+                          tol_backtracking, x_ast=None, p_ast=None, maxiter=30,
+                          mu=None,
+                          gf_symbolic=None, Hf_symbolic=None,
+                          tol_outer_iter=1e-6,maxiter_path=30
+                          ):
+    m = len(constraints_ineq.keys())
+    if p_ast:
+        t_0 = m/math.fabs(f(x_0)-p_ast) #other option: t_0 = mu*m/(f(x_0)-p_ast)
+    else:
+        print("p_ast must be defined")
+    t = t_0
+    x = x_0
+    log_barrier_eval = logarithmic_barrier(f,x,t,constraints_ineq)
+    stopping_criteria = m/t
+    outer_iter = 1
+    const_ineq_funcs_eval = constraint_inequalities_funcs_eval(x,constraints_ineq)
+        
+    print("Outer iterations of path following method")
+    print('{} {:0.2e}'.format("Mu value:", mu))
+    print('Outer iteration\tLogBarrier \tt_log_barrier\tStopping criteria')
+    print('{}\t\t{:0.2e}\t{:0.2e}\t{:0.2e}'.format(outer_iter,log_barrier_eval,
+                                                 t,stopping_criteria
+                                                 ))
+    print("----------------------------------------------------------------------------------------")
+    iter_barrier = 0
+    while(stopping_criteria > tol_outer_iter and outer_iter < maxiter_path):
+        [x,iteration,Err_plot,x_plot] = Newtons_method_log_barrier_feasible_init_point(f, A, constraints_ineq, 
+                                                                                       t, x_0, tol, 
+                                                                                       tol_backtracking, x_ast, p_ast,
+                                                                                       maxiter,
+                                                                                       gf_symbolic, Hf_symbolic)
+        print("Inner iterations")
+        print(x)
+        const_ineq_funcs_eval = constraint_inequalities_funcs_eval(x,constraints_ineq)
+        
+        if(sum(const_ineq_funcs_eval > np.nextafter(0,1)) >=1):
+            print("Some constraint inequalities evaluated in x were nonnegative, check approximations")
+        
+        t=mu*t
+        log_barrier_eval = logarithmic_barrier(f,x,t,constraints_ineq)
+        outer_iter+=1
+        stopping_criteria = m/t
+        print("----------------------------------------------------------------------------------------")
+        print("Outer iterations of path following method")
+        print('{} {:0.2e}'.format("Mu value:", mu))
+        print('Outer iteration\tLogBarrier \tt_log_barrier\tStopping criteria')
+        print('{}\t\t{:0.2e}\t{:0.2e}\t{:0.2e}'.format(outer_iter,log_barrier_eval,
+                                                     t,stopping_criteria
+                                                     ))
+        print("----------------------------------------------------------------------------------------")
+        iter_barrier+= iter_barrier + iteration
+            
+    return [x,iter_barrier,t]
+def Newtons_method_log_barrier_feasible_init_point(f, A, constraint_inequalities, 
+                                                   t_path, x_0, tol, 
+                                                   tol_backtracking, x_ast=None, p_ast=None, maxiter=30,
+                                                   gf_symbolic = None,
+                                                   Hf_symbolic = None):
+    '''
+    Newton's method to numerically approximate solution of min f subject to Ax = b.
+    IMPORTANT: this implementation requires that initial point x_0, satisfies: Ax_0 = b
+    Args:
+        f (fun): definition of function f as lambda expression or function definition.
+        A (numpy ndarray): 2d numpy array of shape (m,n) defines system of constraints Ax=b.
+        
+        constraints_ineq
+        
+        t_path
+        
+        x_0 (numpy ndarray): initial point for Newton's method. Must satisfy: Ax_0 = b
+        tol (float): tolerance that will halt method. Controls stopping criteria.
+        tol_backtracking (float): tolerance that will halt method. Controls value of line search by backtracking.
+        x_ast (numpy ndarray): solution of min f, now it's required that user knows the solution...
+        p_ast (float): value of f(x_ast), now it's required that user knows the solution...
+        maxiter (int): maximum number of iterations
+        gf_symbolic (fun): definition of gradient of f. If given, no approximation is
+                                     performed via finite differences.
+        Hf_symbolic (fun): definition of Hessian of f. If given, no approximation is
+                                     performed via finite differences.
+    Returns:
+        x (numpy ndarray): numpy array, approximation of x_ast.
+        iteration (int): number of iterations.
+        Err_plot (numpy ndarray): numpy array of absolute error between p_ast and f(x) with x approximation
+                          of x_ast. Useful for plotting.
+        x_plot (numpy ndarray): numpy array that containts in columns vector of approximations. Last column
+                        contains x, approximation of solution. Useful for plotting.
+    '''
+    iteration = 0
+        
+    x = x_0
+    
+    feval = f(x)
+        
+    if gf_symbolic and Hf_symbolic:
+        gfeval = gf_symbolic(x)
+        Hfeval = Hf_symbolic(x)
+    else:
+        grad_Hess_log_barrier_dict = numerical_differentiation_of_logarithmic_barrier(f,x,t_path,
+                                                                                      constraint_inequalities)
+        gfeval = grad_Hess_log_barrier_dict['gradient']
+        Hfeval = grad_Hess_log_barrier_dict['Hessian']
+
+    
+    normgf = np.linalg.norm(gfeval)
+    condHf= np.linalg.cond(Hfeval)
+    
+    Err_plot_aux = np.zeros(maxiter)
+    Err_plot_aux[iteration]=compute_error(p_ast,feval)
+    
+    Err = compute_error(x_ast,x)
+    
+    if (np.all(A < np.nextafter(0,1))): #A is zero matrix
+        system_matrix = Hfeval
+        rhs = -gfeval
+        n = x.size
+        p = 0
+        
+    else:
+        if(A.ndim == 1):
+            p = 1
+            n = x.size
+            zero_matrix = np.zeros(p)
+            first_stack = np.column_stack((Hfeval, A.T))
+            second_stack = np.row_stack((A.reshape(1,n).T,zero_matrix)).reshape(1,n+1)[0]
+        else:
+            p,n = A.shape
+            zero_matrix = np.zeros((p,p))
+            first_stack = np.column_stack((Hfeval, A.T))
+            second_stack = np.column_stack((A,zero_matrix))
+            
+        system_matrix = np.row_stack((first_stack,second_stack))
+        zero_vector = np.zeros(p)
+        rhs = -np.row_stack((gfeval.reshape(n,1), zero_vector.reshape(p,1))).T[0]
+    
+    x_plot = np.zeros((n,maxiter))
+    x_plot[:,iteration] = x
+    
+
+    #Newton's direction and Newton's decrement
+    dir_desc = np.linalg.solve(system_matrix, rhs)
+    dir_Newton = dir_desc[0:n]
+    dec_Newton = -gfeval.dot(dir_Newton)
+    w_dual_variable_estimation = dir_desc[n:(n+p)]
+
+
+    print('I\tNorm gfLogBarrier \tNewton Decrement\tError x_ast\tError p_ast\tline search\tCondHf')
+    print('{}\t\t{:0.2e}\t\t{:0.2e}\t{:0.2e}\t{:0.2e}\t{}\t\t{:0.2e}'.format(iteration,normgf,
+                                                                         dec_Newton,Err,
+                                                                         Err_plot_aux[iteration],"---",
+                                                                         condHf))
+    
+    stopping_criteria = dec_Newton/2
+    iteration+=1
+    while(stopping_criteria>tol and iteration < maxiter):
+        der_direct = -dec_Newton
+        t = line_search_for_log_barrier_by_backtracking(f,dir_Newton,x,t_path,
+                                                        constraint_inequalities,
+                                                        der_direct)
+        x = x + t*dir_Newton
+        feval = f(x)
+            
+        if gf_symbolic and Hf_symbolic:
+            gfeval = gf_symbolic(x)
+            Hfeval = Hf_symbolic(x)
+        else:
+            grad_Hess_log_barrier_dict = numerical_differentiation_of_logarithmic_barrier(f,x,t_path,
+                                                                                          constraint_inequalities)
+            gfeval = grad_Hess_log_barrier_dict['gradient']
+            Hfeval = grad_Hess_log_barrier_dict['Hessian']
+        
+        if (np.all(A < np.nextafter(0,1))): #A is zero matrix
+            system_matrix = Hfeval
+            rhs = -gfeval
+            n = x.size
+            p = 0
+        else:
+            if(A.ndim == 1):
+                first_stack = np.column_stack((Hfeval, A.T))
+            else:
+                first_stack = np.column_stack((Hfeval, A.T))
+            system_matrix = np.row_stack((first_stack,second_stack))
+            rhs = -np.row_stack((gfeval.reshape(n,1), zero_vector.reshape(p,1))).T[0]
+
+        #Newton's direction and Newton's decrement
+        dir_desc = np.linalg.solve(system_matrix, rhs)
+        dir_Newton = dir_desc[0:n]
+        dec_Newton = -gfeval.dot(dir_Newton)
+        w_dual_variable_estimation = dir_desc[n:(n+p)]
+        
+        Err_plot_aux[iteration]=compute_error(p_ast,feval)
+        x_plot[:,iteration] = x
+        Err = compute_error(x_ast,x)
+        print('{}\t\t{:0.2e}\t\t{:0.2e}\t{:0.2e}\t{:0.2e}\t{:0.2e}\t{:0.2e}'.format(iteration,normgf,
+                                                                                dec_Newton,Err,
+                                                                                Err_plot_aux[iteration],t,
+                                                                                condHf))
+        stopping_criteria = dec_Newton/2
+        if t<tol_backtracking: #if t is less than tol_backtracking then we need to check the reason
+            iter_salida=iteration
+            iteration = maxiter - 1
+        iteration+=1
+    print('{} {:0.2e}'.format("Error of x with respect to x_ast:",Err))
+    print('{} {}'.format("Approximate solution:", x))
+    cond = Err_plot_aux > np.finfo(float).eps*10**(-2)
+    Err_plot = Err_plot_aux[cond]
+    
+    if iteration == maxiter and t < tol_backtracking:
+        print("Backtracking value less than tol_backtracking, check approximation")
+        iteration=iter_salida
+    else:
+        if iteration == maxiter:
+            print("Reached maximum of iterations, check approximation")
+    x_plot = x_plot[:,:iteration]
+    return [x,iteration,Err_plot,x_plot]
