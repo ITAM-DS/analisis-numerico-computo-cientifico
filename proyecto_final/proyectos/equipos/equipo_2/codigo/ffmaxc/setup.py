c55f9920dcff4553f87cea6986f36f7ff5c0735b
Dira Martínez Guzmán <69265402+diramtz@users.noreply.github.com>
2021-05-14 17:38:24 -0500

    subiendo código

---
+++ proyecto_final/proyectos/equipos/equipo_2/codigo/ffmaxc/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(ext_modules = cythonize("ffmaxc.pyx", 
+                              compiler_directives={'language_level' : 3}))
