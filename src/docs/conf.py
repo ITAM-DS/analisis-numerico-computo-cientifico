2588c8852536716cbcccdc6b47e060a9e259f089
erick <palmoreck@gmail.com>
2022-01-21 10:25:55 -0600

    0.2 opt version

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -22,7 +22,7 @@ author = '"Erick Palacios Moreno"'
 master_doc = 'index'
 add_module_names = False
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.2'
 
 
 # -- General configuration ---------------------------------------------------
--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="opt",
-      version="0.1",
+      version="0.2",
       description=u"Package for educational purposes :)",
       url="",
       author="Erick Palacios Moreno -- palmoreck --",
